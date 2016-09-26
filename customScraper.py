e79f112b2ee54cbbe721bbea7032d6099df86c1b
Liam Gensel <liamgens@gmail.com>
2016-09-26 13:43:33 -0400

    working customScraper

--- customScraper.py
+++ customScraper.py
@@ -3,7 +3,7 @@ from BeautifulSoup import BeautifulSoup
 import boto3
 
 sns = boto3.client('sns')
-number = '+19179033471'
+arn = 'arn:aws:sns:us-west-2:152022601810:C3_Menu'
 
 response = requests.get('https://myubcard.com/dining/menu')
 html = response.content
@@ -20,5 +20,4 @@ for ul in menuDiv.find('ul'):
         listOfDinner.append(li.text)
         menu += li.text + '\n'
 
-sns.publish(PhoneNumber=number, Message=menu)
-# sns.publish(TopicArn='arn:aws:sns:us-west-2:152022601810:C3_Menu', Message=menu)
+sns.publish(TopicArn=arn, Message=menu)
