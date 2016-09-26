59cbb11c9b846df342db1f7319e898e9622557d0
Liam Gensel <liamgens@gmail.com>
2016-09-26 14:56:07 -0400

    working

--- customScraper.py
+++ customScraper.py
@@ -1,6 +1,12 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 import boto3
+import time
+
+# TODO add the date in the heading
+# TODO set an automatic time to run
+# TODO add multiple dining centers
+# TODO add headings
 
 sns = boto3.client('sns')
 arn = 'arn:aws:sns:us-west-2:152022601810:C3_Menu'
@@ -9,15 +15,12 @@ response = requests.get('https://myubcard.com/dining/menu')
 html = response.content
 soup = BeautifulSoup(html)
 menuDiv = soup.find('div', attrs={'id': 'menu-56-dinner'})
-dinnerItem = menuDiv.findAll('li', attrs={'class': 'item-li dinner-border'})
 
-listOfDinner = []
-menu = ''
+menu = '\n\n'
 
 for ul in menuDiv.find('ul'):
     for li in ul.findAll('li', attrs={'class': 'item-li dinner-border'}):
-        print li.text
-        listOfDinner.append(li.text)
-        menu += li.text + '\n'
-
+        menu += '-' + li.text + '\n'
+menu += '\n' + time.strftime("%m/%d/%Y") + ' ' + time.strftime("%I:%M:%S %p")
+print menu
 sns.publish(TopicArn=arn, Message=menu)
