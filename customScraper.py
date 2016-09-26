a8a954857b62f4becfdfec8152daa8623606427f
Liam Gensel <liamgens@gmail.com>
2016-09-26 15:27:14 -0400

    Update customScraper.py

--- customScraper.py
+++ customScraper.py
@@ -2,11 +2,25 @@ import requests
 from BeautifulSoup import BeautifulSoup
 import boto3
 import time
+import datetime
 
 # TODO add the date in the heading
 # TODO set an automatic time to run
 # TODO add multiple dining centers
 # TODO add headings
+
+
+def dayOfWeek(day):
+    switcher = {
+        0: 'Monday',
+        1: 'Tuesday',
+        2: 'Wednesday',
+        3: 'Thursday',
+        4: 'Friday',
+        5: 'Saturday',
+        6: 'Sunday',
+    }
+    return switcher.get(day, "DAY_INVALID")
 
 sns = boto3.client('sns')
 arn = 'arn:aws:sns:us-west-2:152022601810:C3_Menu'
@@ -16,7 +30,8 @@ html = response.content
 soup = BeautifulSoup(html)
 menuDiv = soup.find('div', attrs={'id': 'menu-56-dinner'})
 
-menu = '\n\n'
+menu = '\n\nTonight, ' + dayOfWeek(datetime.datetime.today().weekday()) + ' ' + \
+    time.strftime("%m/%d") + ' @ C3\n'
 
 for ul in menuDiv.find('ul'):
     for li in ul.findAll('li', attrs={'class': 'item-li dinner-border'}):
