2a34b774a9a778f4cc7f1b75621b86b0f9f614ba
Liam Gensel <liamgens@gmail.com>
2016-09-26 13:33:36 -0400

    customScraper.py fixed

--- customScraper.py
+++ customScraper.py
@@ -2,6 +2,9 @@ import requests
 from BeautifulSoup import BeautifulSoup
 import boto3
 
+sns = boto3.client('sns')
+number = '+19179033471'
+
 response = requests.get('https://myubcard.com/dining/menu')
 html = response.content
 soup = BeautifulSoup(html)
@@ -9,11 +12,13 @@ menuDiv = soup.find('div', attrs={'id': 'menu-56-dinner'})
 dinnerItem = menuDiv.findAll('li', attrs={'class': 'item-li dinner-border'})
 
 listOfDinner = []
+menu = ''
 
 for ul in menuDiv.find('ul'):
     for li in ul.findAll('li', attrs={'class': 'item-li dinner-border'}):
         print li.text
         listOfDinner.append(li.text)
+        menu += li.text + '\n'
 
-publishAmazonSnsMsg('Dinner', 'arn:aws:sns:us-west-2:152022601810:C3_Menu',
-                    'Ass for Days', 'AKIAI4X4J46SHFPQ6QWQ', 'vafJzXS9xmPmV8T6P/jZVYVhtra7acRC3EOdpRLE')
+sns.publish(PhoneNumber=number, Message=menu)
+# sns.publish(TopicArn='arn:aws:sns:us-west-2:152022601810:C3_Menu', Message=menu)
