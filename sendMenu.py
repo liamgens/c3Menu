c258acb0c192ef480f793d3c2c2142cf5e81e85d
Liam Gensel <liamgens@gmail.com>
2016-09-27 11:27:06 -0400

    Added one script to send all menus

--- sendMenu.py
+++ sendMenu.py
@@ -21,23 +21,56 @@ def dayOfWeek(day):
         5: 'Saturday',
         6: 'Sunday',
     }
-    return switcher.get(day, "DAY_INVALID")
+    return switcher.get(day, 'DAY_INVALID')
+
+
+def topicSelect(location):
+    switcher = {
+        '0': 'arn:aws:sns:us-west-2:152022601810:C3_Menu',
+        '1': 'arn:aws:sns:us-west-2:152022601810:MS_Menu',
+        '2': 'arn:aws:sns:us-west-2:152022601810:GOV_Menu',
+    }
+    return switcher.get(location, 'arn:aws:sns:us-west-2:152022601810:C3_Menu')
+
+
+def menuSelect(location):
+    switcher = {
+        '0': 'menu-56-dinner',
+        '1': 'menu-9-dinner',
+        '2': 'menu-29-dinner',
+    }
+    return switcher.get(location, 'menu-56-dinner')
+
+
+def locationSelect(location):
+    switcher = {
+        '0': 'C3',
+        '1': 'South',
+        '2': 'Governor\'s',
+    }
+    return switcher.get(location, 'C3')
+
 
 sns = boto3.client('sns')
-arn = 'arn:aws:sns:us-west-2:152022601810:C3_Menu'
+loc = raw_input(
+    '\n0 for Crossroads Culinary Center\n1 for Main Street Market Dining Center\n2 for Governors Dining Center\n\nWhich one? ')
+arn = topicSelect(loc)
 
 response = requests.get('https://myubcard.com/dining/menu')
 html = response.content
 soup = BeautifulSoup(html)
-menuDiv = soup.find('div', attrs={'id': 'menu-56-dinner'})
+menuDiv = soup.find('div', attrs={'id': menuSelect(loc)})
 
 menu = '\nTonight, ' + dayOfWeek(datetime.datetime.today().weekday()) + ' ' + \
-    time.strftime("%m/%d") + ' @ C3\n\n'
+    time.strftime("%m/%d") + ' @ ' + locationSelect(loc) + '\n\n'
 
 for ul in menuDiv.find('ul'):
     for li in ul.findAll('li', attrs={'class': 'item-li dinner-border'}):
         menu += '-' + li.text + '\n'
+
 menu += '\nText "STOP" to unsubscribe\n' + \
     time.strftime("%m/%d/%Y") + ' ' + time.strftime("%I:%M:%S %p")
+
 print menu
+
 sns.publish(TopicArn=arn, Message=menu)
